d4fe082fac17a674692bd07cf9bfb877c4b6cda8
Mohsen Banan <form@mohsen.banan.1.byname.net>
2024-10-08 20:19:37 -0700

    emacs30  cl-third etc.

--- blee-lib-general.el
+++ blee-lib-general.el
@@ -218,6 +218,11 @@ find-file-noselect prompts if $baseDir does not exist.
     )
   )
 
+;;;
+;;; call-stack and compile-time-function-name are taken from:
+;;; https://emacs.stackexchange.com/questions/2310/can-functions-access-their-name
+;;;
+
 (defun call-stack ()
   "Return the current call stack frames."
   (let ((frames)
@@ -225,20 +230,19 @@ find-file-noselect prompts if $baseDir does not exist.
         (index 5))
     (while (setq frame (backtrace-frame index))
       (push frame frames)
-      (incf index))
-    (remove-if-not 'car frames)))
+      (cl-incf index))
+    (cl-remove-if-not 'car frames)))
 
 (defmacro compile-time-function-name ()
   "Get the name of calling function at expansion time."
   (symbol-name
    (cadadr
-    (third
-     (find-if (lambda (frame) (ignore-errors (equal (car (third frame)) 'defalias)))
+    (cl-third
+     (cl-find-if (lambda (frame) (ignore-errors (equal (car (cl-third frame)) 'defalias)))
               (reverse (call-stack)))))))
 
 ;; (my-test-function)
 ;;(defun my-test-function ()  (message "This function is named '%s'" (compile-time-function-name)))
-
 
 
 (defun b:insert-file-contents|forward (<fileName)
